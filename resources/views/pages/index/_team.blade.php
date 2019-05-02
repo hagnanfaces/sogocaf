a592b9db8f9cbea45738970dc119dc6aba5a9c2f
ad maj <aka.majoie@gmail.com>
2019-05-02 15:29:14 +0000

    fixed some tipo to team

--- resources/views/pages/index/_team.blade.php
+++ resources/views/pages/index/_team.blade.php
@@ -33,71 +33,8 @@
                     </div>
                 </div>
             </div>
-            <!-- /team -->-->
+            <!-- /team -->
             @endforeach
-
-            {{--<!-- team -->
-            <div class="col-sm-4">
-                <div class="team">
-                    <div class="team-img">
-                        <img class="img-responsive" src="{{ asset('assets/img/dg.jpg') }}" alt="">
-                        <div class="overlay">
-                            <div class="team-social">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-google-plus"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-content">
-                        <h3>Mr Charbel GHOUSSOUB</h3>
-                        <span>Directeur Général</span>
-                    </div>
-                </div>
-            </div>
-            <!-- /team -->
-
-            <!-- team -->
-            <div class="col-sm-4">
-                <div class="team">
-                    <div class="team-img">
-                        <img class="img-responsive" src="{{ asset('assets/img/team5.jpg') }}" alt="">
-                        <div class="overlay">
-                            <div class="team-social">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-google-plus"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-content">
-                        <h3>STAFF</h3>
-                        <span>Administration</span>
-                    </div>
-                </div>
-            </div>
-            <!-- /team -->
-
-            <!-- team -->
-            <div class="col-sm-4">
-                <div class="team">
-                    <div class="team-img">
-                        <img class="img-responsive" src="{{ asset('assets/img/team4.jpg') }}" alt="">
-                        <div class="overlay">
-                            <div class="team-social">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-google-plus"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-content">
-                        <h3>STAFF</h3>
-                        <span>Usine</span>
-                    </div>
-                </div>
-            </div>
-            <!-- /team -->--}}
 
         </div>
         <!-- /Row -->
